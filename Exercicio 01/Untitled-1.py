aa00ab2c347a39f0edd375d9bc883b0b02a26b67
H1d3l <hidels.rios@gmail.com>
2018-09-25 10:10:47 -0300

    quest02 finalizada

--- Exercicio 01/Untitled-1.py
+++ Exercicio 01/Untitled-1.py
@@ -1,4 +1,7 @@
 import requests
+from PIL import Image
+from StringIO import StringIO
+
 #response = requests.get('http://www.google.com')
 
 def getstatuscode(url):
@@ -13,9 +16,13 @@ def getheaderscontentlength(url):
     response = requests.get(url) 
     return response.headers['content-length']
 
+def getimage(url):
+    response = requests.get(url)
+    i = Image.open(StringIO(response.content))
+    i.save("flower.jpg")
+    return i.show()   
 
-
-
+#getimage("https://conteudo.imguol.com.br/c/noticias/35/2018/09/22/22set2018---o-candidato-do-pt-fernando-haddad-caminhada-no-centro-do-recife-acompanhado-da-sua-vice-manuela-davila-pcdob-do-senador-humberto-costa-e-do-candidato-a-reeleicao-ao-governo-1537637710639_615x300.jpg")
      
 def main():
 
@@ -27,6 +34,9 @@ def main():
             print "statuscode:",getstatuscode(op)
             print "cabecalho:",getheaderscontentype(op)
             print "content length:",getheaderscontentlength(op)
+        elif menu == 2:
+            op = raw_input("url:")
+            getimage(op)
 
    
 
