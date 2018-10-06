a7fc9d79b907cc7a9d338dc9a834167553f96533
H1d3l <hidels.rios@gmail.com>
2018-10-06 09:43:54 -0300

    outros

--- Trabalho 01/buscador.py
+++ Trabalho 01/buscador.py
@@ -3,33 +3,38 @@ import requests,re
 
 class Buscador:
     
-    def __init__(self,keyword):
+    def __init__(self,keyword,url,deth):
         self.keyword = keyword
+        self.url = url
+        self.deth = deth
 
-    def search_word(self,url):
+    def search_word(self):
         words = []
+        links = []
         try:
-            response = requests.get(url)
+            response = requests.get(self.url)
         except Exception as erro:
             print('Erro de Conexão')
             response = None
+
         soup = BeautifulSoup(response.text,'html.parser')
         temp = soup.find_all(string = re.compile(self.keyword))
-        for i in temp:
-            temp1 = i.find(self.keyword)
-            words.append(i[temp1-10:len(self.keyword)+temp1+10])
-        return words
-
-
-    def search_all(self,url,deth):
-        links = []
-        if deth == 0:
-            return self.search_word(url)
         
-        #if deth>0:
+        if self.deth == 0:                
+            for i in temp:
+                temp1 = i.find(self.keyword)
+                words.append(i[temp1-10:len(self.keyword)+temp1+10])
+            return words
+        elif self.deth>0:
+            for i in soup.find_all('a'):
+                links.append(i.get('href'))
+                
+
+
+
 
 
                 
 
-p = Buscador('Hildebrando')
-print(p.search_all('https://pt.wikipedia.org/wiki/Hildebrando_Pascoal',0))
+p = Buscador('Hildebrando','https://pt.wikipedia.org/wiki/Hildebrando_Pascoal',0)
+print(p.search_word())
