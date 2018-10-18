85e7b5209888bc174bf904cb30ab6359ae5aa9f2
H1d3l <hidels.rios@gmail.com>
2018-10-18 15:33:36 -0300

    projeto django

---
+++ Django/Projeto#01/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hidel/Projetos/D.P.Internet_2018.2/Django/Projeto#01/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
